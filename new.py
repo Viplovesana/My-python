618d41e1dbe0ac30ee30f62b8f79a01db976f4ea
Viplovesana <viplovesana90@gmail.com>
2025-03-03 13:17:00 +0530

    intial commit

---
+++ new.py
@@ -0,0 +1,15 @@
+
+print("hello")
+print(1)
+print([10,20,30,40])
+print((2,5,7))
+print({'viplove','rahul','age',24})
+print({'neeraj',20,30,40}
+)
+
+name = input("enter your name : ")
+print(name)
+
+name = input("enter your age : ")
+print(name)
+print(type(name))
