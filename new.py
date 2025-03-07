108525398be99a9abfecc48cd20799f83d8fcf05
Viplovesana <viplovesana90@gmail.com>
2025-03-07 09:34:48 +0530

    new commit

--- new.py
+++ new.py
@@ -1,15 +1,189 @@
 
-print("hello")
-print(1)
-print([10,20,30,40])
-print((2,5,7))
-print({'viplove','rahul','age',24})
-print({'neeraj',20,30,40}
-)
+# print("hello")
+# print(1)
+# print([10,20,30,40])
+# print((2,5,7))
+# print({'viplove','rahul','age',24})
+# print({'neeraj',20,30,40}
+# )
 
-name = input("enter your name : ")
-print(name)
+# name = input("enter your name : ")
+# print(name)
 
-name = input("enter your age : ")
-print(name)
-print(type(name))
+# name = input("enter your age : ") 
+
+# print(type(name))
+
+
+
+
+
+# x = 10
+# print(id(x))
+
+# y = 10
+# print(id(y))
+
+# x = 10 
+
+# y = 10
+
+# print(id(x),id(y))
+
+
+# 
+
+
+# x = 65
+# print(chr(65))
+
+
+    # x="raj"
+    # y="raj"
+    # print(id(x),id(y))
+
+
+    # l1 = [10,30,50]
+    # l2 = [10,30,50]
+    # print(id(l1),id(l2))
+
+# x=(10,20,30)
+# y=(10,20,30)
+# print(id(x),id(y))
+
+# d1 = {"viplove","neeraj"}
+# d2 = {"viplove","neeraj"}
+# print(id(d1),id(d2))
+
+
+# s1 = {10,20,30}
+# s2 = {10,20,30}
+# print(id(s1),id(s2))
+
+# f1 = frozenset([10,20,30])
+# f2 = frozenset([10,20,30])
+# print(id(f1),id(f2))
+
+
+
+# x = 12345
+# y = x//10
+# z = x%10
+# print(y)
+# print(z)
+
+
+# print(x>y)
+
+# x = int(input("enter 1st value"))
+# y = int(input("enter 2nd value"))
+
+
+# ..........................assignment oparator............................
+
+# x = 10
+# x+=1
+# print(x)
+# x-=1
+# print(x)
+# x*=2
+# print(x)
+# x/=2
+# print(x)
+# x%=2
+# print(x)
+
+
+
+# ...........................Logical Oprator..................................................
+
+
+# x=10
+# y=20
+# z=30
+
+# ...........and oprator...............
+
+# print(x>y and y<z)
+# print(x<y and y<z)
+# print(x>y and y<z)
+# print(x<y and y>z)
+
+# # ..........or operator................
+
+# print(x>y or y>z)
+# print(x<y or y<z)
+# print(x>y or y<z)
+# print(x<y or y>z)
+
+# ...........not oprator..............
+
+
+# print(not(x>y or y>z))
+# print(not(x<y or y<z))
+
+
+
+
+# ..........................membership Operator...................................
+
+
+# l1 = [10,20,30,'Neeraj','raj']
+# print(10 in l1)
+
+# print('raj' in l1)
+
+# print('Jai' in l1)
+
+# s1 = "Neeraj"
+# print('E' in s1)
+
+
+
+
+# ........................identity operator.......................................
+
+
+# x=10
+# y=20
+# print(x is not y)
+
+# x=10
+# y="10"
+# print(x is not y)  
+
+# x=10
+# y="10"
+# print(x==y)
+
+
+# .............................bitwise operator....................................
+
+
+# and...........................&
+# or..........................|
+# bitwise not...................~
+# bitwise x-or...................^
+# left sift....................<<
+# right shift.....................>>
+
+
+# x=10
+# print(bin(x))
+# print(oct(x))
+# print(hex(x))
+
+
+x = 10
+y = 15
+print(x | y)
+print(x & y)
+print(x ~ y)
+print(x ^ y)
+print(x << y)
+print(x >> y)
+
+
+
+
+
