9f9bd7eded2bdea5b97bb550f04febdc86eb2c0a
Viplovesana <viplovesana90@gmail.com>
2025-04-06 12:33:26 +0530

    new add

--- Control-flow-statement.py/if_else.py
+++ Control-flow-statement.py/if_else.py
@@ -9,24 +9,7 @@
 
 
 
-
-
-# x = int(input("enter the no :"))
-
-# if x%2==0:
-#     print(f'given no {x} is even')
-
-
-# age = int(input('enter your age : '))
-
-# if age < 18:
-#     print('your are to young to marry')
-# elif age > 50:
-#     print('you ared to old for marry')
-# else:
-#     print('your are the perfect match')  
-
-
+'even odd'
 
 # n = int(input('Enter the no : '))
 
@@ -38,7 +21,7 @@
 
 
 
-#take threee no as input and print the largest one using if-elif-else
+'take threee no as input and print the largest one using if-elif-else'
 
 
 # x = int(input('enter the first no :'))
@@ -54,6 +37,9 @@
 #     print(f"the largest no is {z}")        
 
 
+'sum of x and y................'
+
+
 # x = int(input("enter first no :"))
 
 # y = int(input("enter second no :"))
@@ -63,22 +49,6 @@
 # print(f"sum of {x} + {y} = {z}")
 
 
-# x = int(input("enter first no :"))
-
-# y = int(input("enter second no :"))
- 
-# x ,y  = y , x
-
-# print(f"value of x = {x} and value of y = {y}")
-
-
-
-# x = int (input("enter any no :"))
-
-# if x%2==0:
-#     print(f'given no {x} is even')
-# else:
-#     print(f'given no of {x} is not even') 
 
 
 # x = int(input("enter first no :"))
@@ -98,7 +68,10 @@
 #      print(f"given no {x} is positive")
 # elif x<=0:
 #      print(f"given no {x} is nagative")
-          
+ 
+
+
+
 # x = int(input("enter the first no"))
 # y = int(input("enter the second no"))
 # z = int(input("enter the third no"))
@@ -108,19 +81,64 @@
 #      print
 
 
+
+
+
+
+
 'cheak if a year is a leap year ......................................'
 
 
-year = int(input('enter the yearv : '))
+# year = int(input('enter the yearv : '))
 
-if year % 4 == 0:
-    print(f"{year} is a leap year")
-elif year % 100 == 0:
-    print(f"{year} is not a leap year") 
-elif year % 400 ==0:
-    print(f"{year} is a leap year")
-else:
-    print(f"{year} i not leap year")           
+# if year % 4 == 0:
+#     print(f"{year} is a leap year")
+# elif year % 100 == 0:
+#     print(f"{year} is not a leap year") 
+# elif year % 400 ==0:
+#     print(f"{year} is a leap year")
+# else:
+#     print(f"{year} i not leap year")           
 
 
+
+'marriage age ......................................'
+
+
+
+# age = int(input('enter your age : '))
+
+# if age < 18:
+#     print('you are to young to marry')
+# elif age > 50:
+#     print('you ared to old to marry')
+# else:
+#     print('your are the perfect match')  
+
+
+
+"positive and negative.........................................."
+
+# n = int(input('enter any no : '))
+
+# if n == 0:
+#     print(f"{n} given no is zero ")  
+# elif n >= 0:
+#     print(f"{n} is a positive no")
+# else:
+#     print(f"{n} is negative no")          
    
+
+
+
+"enter changing the value ..................................."
+
+
+# x = int(input("enter first no :"))
+
+# y = int(input("enter second no :"))
+ 
+# x ,y  = y , x
+
+# print(f"value of x = {x} and value of y = {y}")
+
