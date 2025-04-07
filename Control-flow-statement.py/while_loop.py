a01395e1f241039664dcc5952f68e63571f95809
Viplovesana <viplovesana90@gmail.com>
2025-04-08 01:14:23 +0530

    new commit

--- Control-flow-statement.py/while_loop.py
+++ Control-flow-statement.py/while_loop.py
@@ -77,3 +77,83 @@
 # print ('loop ended')
 
 
+
+'WAP to print a no from 1 to 100'
+
+
+# i =  1
+
+# while i <= 100:
+#     print(i)
+#     i = i + 1
+
+
+# i =  100
+# while i>=1:
+#     print(i)
+#     i = i - 1
+# print('loop ended')   
+# 
+     
+
+
+'WAP to print multiplication of n no....'
+
+# n = int(input('enter any no :'))
+
+# i =  1n
+# while i <= 10:
+#     print(n*i)
+#     i = i + 1
+
+
+
+
+'WAP to print pelindrom of a string '
+
+'wap to print ar'
+
+# n = int(input("enter any no : "))
+
+# digit_count = 0
+# while n>0:
+#     n = n // 10
+#     digit_count = digit_count +1
+# print(digit_count)    
+
+
+
+'print thre elements of the following list using a loop'
+
+# num = [5,10,15,20,25,30,35,40,45,50]
+
+# index = 0
+
+# while index<len(num):
+#     print(num[index])
+#     index=index+1
+
+# heros=['ironman','superman','thor','shaktiman']
+
+# idx =0
+# while idx<len(heros):
+#     print(idx)
+#     idx=idx+1
+
+
+num = (5,10,15,20,25,30,35,40,45,50)
+x = 50
+i = 0
+
+while i<len(num):
+    if(num[i]==x):
+        print("found at idx",i)
+    else:
+        print('not found')
+    
+    
+    i = i +1
+
+
+
+
