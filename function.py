f5e8bd5c4b7a72bc2fda032b7fdae012a132dbba
Viplovesana <viplovesana90@gmail.com>
2025-04-16 10:46:10 +0530

    new commit

---
+++ function.py
@@ -0,0 +1,34 @@
+
+
+# def add(x,y):
+#     return (x+y)
+
+# p = int(input("enter any number :"))
+# q = int(input("enter any number :")) 
+
+
+# z = add(p,q)
+# print(z)
+# print("hello")
+# print(z)
+
+
+
+# def even(n):
+#     for i in range(2,n,2):
+#         print(i)
+# x =int(input("enter a number :"))
+
+
+# even(x)
+
+
+def even_or_not(n):
+    if n%2==0:
+        print(f'given no{n}is even')
+    else:
+        print(f'given no {n}is odd')
+n=int(input("enter any number :"))
+even_or_not(n)            
+
+
