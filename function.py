26b2c4cd82b221a953133c179be348af8cb304b6
Viplovesana <viplovesana90@gmail.com>
2025-04-17 11:28:36 +0530

    new commit

--- function.py
+++ function.py
@@ -23,12 +23,23 @@
 # even(x)
 
 
-def even_or_not(n):
-    if n%2==0:
-        print(f'given no{n}is even')
+# def even_or_not(n):
+#     if n%2==0:
+#         print)
+#     else:
+#         print(f'given no {n}is odd')
+# n=int(input("enter any number :"))
+# even_or_not(n)  
+
+
+
+
+def leap_year(n):
+    if n%4==0 or n%400==0 and n%100!=0:
+     print(f'given no{n}is a leap year')
     else:
-        print(f'given no {n}is odd')
-n=int(input("enter any number :"))
-even_or_not(n)            
+          print(f'given no{n}is not leap year' )
+n =int(input("enter the year :")) 
+leap_year(n)         
 
-
+         
