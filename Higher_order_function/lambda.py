10787594f180bfbc1cfd3feac452f1c0ac7cbec9
Viplovesana <viplovesana90@gmail.com>
2025-04-22 11:40:09 +0530

    new commit

---
+++ Higher_order_function/lambda.py
@@ -0,0 +1,36 @@
+
+
+
+import functools
+
+
+# x = lambda p,q : (p+q)
+
+
+# r = int(input("enter any no"))
+# s =int(input("enter any no"))
+
+# z = x(r,s)
+# print(z)
+
+
+# l =[2,4,6,8,10]
+
+# x = list(map(lambda x: x**2 ,l))
+# print(x)
+
+
+l= [1,2,3,4,5,6,7,8,9]
+# def even_no(x):
+   
+#     if x%2==0:
+#         return x
+# x = filter(even_no,l)
+# print(list(x)) 
+
+# x1 = list(filter(lambda x : x%2==0 ,l))
+# print(x1)
+
+
+x = functools.reduce(lambda a,b:a if a<b else b ,l)
+print(x)
--- Higher_order_function/reduce.py
+++ Higher_order_function/reduce.py
@@ -1,12 +1,22 @@
 
 import functools
 
-l = [10,20,30,50,15,25]
+# l = [10,20,30,50,15,25] 
 
-def max_fan(x,y):
-    if x<y:
-        return x
-    else:
-        return y
-x = functools.reduce(max_fan,l)
-print(x)    
+# def max_fan(x,y):
+#     if x<y:
+#         return x
+#     else:
+#         return y
+# x = functools.reduce(max_fan,l)
+# print(x)    
+
+
+
+l =[1,2,3,4,5,6]
+
+def sum_all(x,y):
+    return x+y
+x = functools.reduce(sum_all,l)
+print(x)
+
