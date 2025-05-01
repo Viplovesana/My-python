24639996d78b1fdaa70efb73118211ea902077e9
Viplovesana <viplovesana90@gmail.com>
2025-05-01 06:15:39 +0530

    file handle

---
+++ my_file_handling.py
@@ -0,0 +1,15 @@
+
+
+f =open('n4.py','w')
+print("file created")
+print(f.name)
+print(f.mode)
+print(f.readable())
+print(f.writable())
+print(f.closed)
+f.close()
+print(f.closed)
+
+
+
+# f =open('x1.py','r')
---
+++ my_generator.py
@@ -0,0 +1,54 @@
+
+# def even(n):
+#     for i in range (1,n+1):
+#         yield (2*i)
+# x = int(input("enter any number :"))
+
+# y = even(x)
+# print(next(y))
+
+# print("hello")
+# print("hi")
+# print(next(y))
+     
+
+
+
+
+
+
+
+
+# def even(n):
+#     for i in range (1,n+1):
+#         yield i
+# x = int(input("enter any number :"))
+
+# y = even(x)
+# print(next(y))     
+# print("hello")
+# print(next (y))
+# print(next(y))
+
+
+def table(n):
+    for i in range(2,n+1):
+        yield i
+
+x= int(input("enter any number:"))
+
+y = table(x)
+# print(len(list(x)))
+z= next(y)
+
+# for i in  range(1,11):
+#     print(f' {z} * {i} = {z*i}')  
+
+z= next(y)
+
+for i in  range(1,11):
+    print(f' {z} * {i} = {z*i}')  
+
+
+
+
