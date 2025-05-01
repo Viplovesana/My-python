4b9879272accf73129805d81de60a8322fb8687d
Viplovesana <viplovesana90@gmail.com>
2025-05-01 06:29:59 +0530

    FILE_HANDLING

--- my_file_handling.py
+++ my_file_handling.py
@@ -1,6 +1,32 @@
 
 
-f =open('n4.py','w')
+# f =open('n4.py','w')
+# print("file created")
+# print(f.name)
+# print(f.mode)
+# print(f.readable())
+# print(f.writable())
+# print(f.closed)
+# f.close()
+# print(f.closed)
+
+
+
+# f =open('x1.py','r')
+
+
+# f =open('n4.py','r')
+# print(f.name)
+# print(f.mode)
+# print(f.readable())
+# print(f.writable())
+# print(f.closed)
+# f.close()
+# print(f.closed)
+
+
+
+f =open('x1.py','a')
 print("file created")
 print(f.name)
 print(f.mode)
@@ -9,7 +35,3 @@ print(f.writable())
 print(f.closed)
 f.close()
 print(f.closed)
-
-
-
-# f =open('x1.py','r')
