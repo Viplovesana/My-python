e39c5a15bfbff3ec4789f55f2025be55c037b44c
Viplovesana <viplovesana90@gmail.com>
2025-05-02 07:27:33 +0530

    new commit

--- my_file_handling.py
+++ my_file_handling.py
@@ -39,17 +39,51 @@
 
 
 
-f =open('x1.txt','w')
-print(f.name)
-print(f.mode)
-print(f.readable())
-print(f.writable())
-print(f.closed)
+# f =open('x1.txt','w')
+# print(f.name)
+# print(f.mode)
+# print(f.readable())
+# print(f.writable())
+# print(f.closed)
 
-data=['python\n','java\n','php\n']
+# data=['python\n','java\n','php\n']
 
-f.writelines(data)
+# f.rea(data)
 
 
 
+# f.close()
+
+
+
+"read operation......."
+
+# f =open('x1.txt')
+
+# print(f.readable())
+# data1 = f.read(5)
+# print(data1)
+# data2 = f.read(7)
+# print(data2)
+
+# f.close()
+
+
+
+f = open ('x1.txt','r')
+f.seek(2,0)
+data= f.read(10)
+print(data)
 f.close()
+
+
+
+
+
+
+
+
+
+
+
+
