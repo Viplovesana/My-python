c39f5dc51a50b4c19c4c76ba8179c5b9912d4cac
Viplovesana <viplovesana90@gmail.com>
2025-05-04 06:40:22 +0530

    new commit

--- Ooopsss.py
+++ Ooopsss.py
@@ -12,19 +12,53 @@
 # print(obj.x)
 
 
-class Home:
-    def __init__(self,name,contact,address):
-        print("constructor called")
-        self.n=name
-        self.c =contact
-        self.a = address
-        print(id(self))
+# class Home:
+#     def __init__(self,name,contact,address):
+#         print("constructor called")
+#         self.n=name
+#         self.c =contact
+#         self.a = address
+#         print(id(self))
 
-obj1 = Home('Name',123,'Bhopal') 
-obj1.__init__('viplove',345,'Bhopal')
-print(id(obj1))
-print(obj1.n)  
-print(obj1.c)  
-print(obj1.a)     
+# obj1 = Home('Name',123,'Bhopal') 
+# obj1.__init__('viplove',345,'Bhopal')
+# print(id(obj1))
+# print(obj1.n)  
+# print(obj1.c)  
+# print(obj1.a)     
     
+
+
+# class Student:
+#     name="viplove sana"
+#     subject="python"
+#     address="cybrom"
+# obj1=Student()
+# print(obj1) 
+# print(obj1.name)   
+# print(obj1.subject)
+# print(obj1.address)
+# obj2=Student()
+# print(obj2.name)
+
+
+
+
+# class Car:
+#     brand="toyota"
+# obj=Car
+# print(obj.brand)   
+
+
+
+class Calculator:
+    def add(x,y):
+        return x+y
+
+object=Calculator
+a=object.add(20,30)
+print(a)
+
+
+
 
