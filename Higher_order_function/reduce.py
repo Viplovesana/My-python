57c3e50b183a2310b51d8c80a8110d909755c7e0
Viplovesana <viplovesana90@gmail.com>
2025-05-17 10:28:01 +0530

    new commit

--- Higher_order_function/reduce.py
+++ Higher_order_function/reduce.py
@@ -12,17 +12,33 @@
 # x = functools.reduce(max_fan,l)
 # print(x) 
 
+# import functools
+
+# l=[10,20,30,40,50,60]
+
+# def max_no(x,y):
+#     if x>y:
+#         return x
+#     else:
+#         return y
+# x=functools.reduce(max_no,l) 
+# print(x)
+
+"print smallest element from the element"
 import functools
 
-l=[10,20,30,40,50,60]
+l = [45,86,24,87,343,897,45,34,6]
 
-def max_no(x,y):
+def small_func(x,y):
     if x>y:
         return x
     else:
         return y
-x=functools.reduce(max_no,l) 
-print(x)
+
+x=functools.reduce(small_func,l) 
+print(x)  
+
+
   
 
 
