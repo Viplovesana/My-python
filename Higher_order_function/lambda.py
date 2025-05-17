8177e367589c5736fa5de715d40512ad606d7ba0
Viplovesana <viplovesana90@gmail.com>
2025-05-18 04:07:27 +0530

    new commit

--- Higher_order_function/lambda.py
+++ Higher_order_function/lambda.py
@@ -45,6 +45,12 @@
 # print (list(square))
 
 "filter method using lambda"
-num=[11,22,33,44,55,66,77,88]
-even=filter(lambda n: n%2==0, num)
-print(list(even))
+# num=[11,22,33,44,55,66,77,88]
+# even=filter(lambda n: n%2==0, num)
+# print(list(even))
+
+"reduce method using lambda"
+import functools
+numbers=[2,3,5,8,3,8,0,4,6,2,7,5,]
+greater=functools.reduce(lambda x,y: x if x>y else y ,numbers )
+print(greater)
