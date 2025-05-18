3b5eadcaa34740686331851fdd83dcee57f85cdf
Viplovesana <viplovesana90@gmail.com>
2025-05-19 03:33:05 +0530

    new commit

--- Ooopsss.py
+++ Ooopsss.py
@@ -556,24 +556,30 @@
 #     std="10th"
 
 # obj=Student()
+
+# obj2=Student()
+# print(Student.std)
 # print(obj.name)
 # print(obj.print)
 # print(obj.std)
 # print(Student.name)    
 
 class Student:
+    ins_name="Cybrom tech"
     name ="viplove sana"
     def __init__(self,name,marks):
         self.fullname=name
         self.marks=marks
-        print("hello how are you")
+        print("hello how are you")         
 S1=Student("viplove",78)
 print(S1.name)
 print(S1.fullname,S1.marks)
+print(S1.ins_name)
 
 S2=Student("jhon",79)
 print(S2.name)
-print(S2.fullname,S2.marks)
+print(S2.fullname,S2.marks) 
+print(S2.ins_name)    
 
 
         
